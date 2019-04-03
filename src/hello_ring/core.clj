d17b05a0480602f885a8fb4dc933b5e9058ccb63
aryyya.xyz@gmail.com <29365565+aryyya@users.noreply.github.com>
2019-04-03 16:56:49 -0700

    Start server listening on PORT environment variable.

--- src/hello_ring/core.clj
+++ src/hello_ring/core.clj
@@ -1,5 +1,19 @@
 (ns hello-ring.core)
 
+(use 'ring.adapter.jetty)
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello, world! From Clojure."})
+
+(defn get-port-or-default
+  [default]
+  (or (Integer/parseInt (System/getenv "PORT"))
+      default))
+
 (defn -main
   []
-  (println "Hello, world! From Clojure."))
+  (let [port (get-port-or-default 8080)]
+    (println (format "Listening for connections on port %d." port))
+    (run-jetty handler {:port port})))
